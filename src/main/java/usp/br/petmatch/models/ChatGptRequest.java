63995f3950fa66bb67fb81c8fb15216de1b0dfb7
amandaperellon <amanda.perellon@gmail.com>
2023-11-26 21:51:14 -0300

    Fixing chatgpt prompt + adding temperature to ChatGptRequest

--- src/main/java/usp/br/petmatch/models/ChatGptRequest.java
+++ src/main/java/usp/br/petmatch/models/ChatGptRequest.java
@@ -10,29 +10,24 @@ public class ChatGptRequest {
     public int max_tokens;
     public ArrayList<Message> messages;
     public String model;
+    public float temperature;
 
     public static final String systemInput = """
-                A partir de agora você irá se comportar como uma API de um aplicativo para adoção 
-                de cachorros. Sua função será receber um input do usuário que é uma breve descrição 
-                do seu dia-a-dia e a partir dessa descrição você deve dizer quais são as características ideais 
-                de um cachorro para essa pessoa. As características e suas classificações são as seguintes: Idade: 
-                Bebê, jovem, adulto e idoso.  Sexo: macho e fêmea. Tamanho: pequeno, médio e grande. Pelagem: Crespo, 
-                curto, médio, longo e cacheado. Castrado: sim e não. Treinado: sim e não. Necessidades especiais: sim e não. 
-                Vacinado: sim e não. Bom com crianças: sim e não. Bom com cachorros: sim e não. Bom com gatos: sim e não. 
-                Para cada um dos atributos escolhidos você deve atribuir um nível de importância, sendo eles: Indiferente, importante, 
-                indispensável. Além disso, você deve incluir também uma breve justificativa para sua escolha (até 20 palavras).
-                Sua resposta deve ser em formato JSON, seguindo o seguinte objeto: {"sexo":"fêmea","importanciaSexo": "Importante",
-                "justificativaSexo": "Fêmeas são mais amorosas"}. Sua resposta deve ser composta exclusivamente pelo objeto JSON. 
-                Cada chave do JSON não pode conter espaço entre as palavras, a primeira letra deve ser minúscula e as outras maiúsculas e 
-                não pode conter acentos. Você tem que necessariamente escolher uma das opções de classificação de característica, 
-                preenchendo todos os atributos e não deixando nenhum em branco.
+            A partir de agora você irá se comportar como uma API de um aplicativo para adoção de cachorros.\s     
+            Você receberá uma breve descrição do dia-a-dia do usuário e deverá classificar a partir das características primárias e secundárias o cachorro ideal para essa pessoa.           
+            As características primárias de classificação e as suas opções são as seguintes: Idade: Bebê, jovem, adulto, idoso; Sexo: macho ou fêmea;Tamanho: pequeno, médio, grande;Pelagem: Crespo, curto, médio, longo e cacheado;Castrado: sim ou não;Treinado: sim ou não;Necessidades especiais: sim ou não;Vacinado: sim ou não;Bom com crianças: sim ou não(o animal precisa gostar de crianças?);Bom com cachorros: sim ou não(o animal precisa gostar de outros cachorros?);Bom com gatos: sim ou não(o animal precisa gostar de gatos?).
+            Cada caraterística primária terá duas secundárias. A primeira é a Importância, que é classificada em indiferente, importante, indispensável. A segunda é a justificativa, que explica a escolha da característica primária em até 10 palavras.\s         
+            Sua resposta deve conter todas as características e ser  composta exclusivamente pelo objeto JSON, seguindo o seguinte modelo de objeto: { "idade":"Bebê", "importanciaIdade": "Importante", "justificativaIdade": "A pessoa deseja um animal filhote"}.  Todas as características devem estar presentes em um único JSON.            
+            Cada chave do JSON não pode conter espaço entre as palavras, a primeira letra deve ser minúscula e não pode conter acentos.\s        
+            Você deve necessariamente escolher uma das opções de classificação dentro das especificadas por cada característica, mesmo que não saiba qual classificar.\s          
             """;
 
-    public static String GetChatGptRequestBody(String model, String userInput, int maxTokens) {
+    public static String GetChatGptRequestBody(String model, String userInput, int maxTokens, float temperature) {
         ObjectMapper om = new ObjectMapper();
         ChatGptRequest obj = new ChatGptRequest();
         obj.max_tokens = maxTokens;
         obj.model = model;
+        obj.temperature = temperature;
 
         var systemMessage = new Message("system", systemInput);
         var userMessage = new Message("user", userInput);
--- src/main/java/usp/br/petmatch/services/ChatGptService.java
+++ src/main/java/usp/br/petmatch/services/ChatGptService.java
@@ -1,7 +1,6 @@
 package usp.br.petmatch.services;
 
-import com.google.gson.Gson;
-import org.json.JSONException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -26,13 +25,15 @@ public class ChatGptService {
     public String CHATGPT_API_MODEL;
     @Value("${chatgpt.api.max.tokens}")
     public String CHATGPT_API_MAX_TOKENS;
+    @Value("${chatgpt.api.temperature}")
+    public String CHATGPT_API_TEMPERATURE;
 
     private final ChatGptRepository chatGptRepository;
-    Gson gson;
+    ObjectMapper om;
 
     public ChatGptService(ChatGptRepository chatGptRepository) {
         this.chatGptRepository = chatGptRepository;
-        this.gson = new Gson();
+        this.om = new ObjectMapper();
     }
 
     public ChatGptModel sendRequest(String userInput){
@@ -40,30 +41,31 @@ public class ChatGptService {
         var apiKey = CHATGPT_API_KEY;
         var model = CHATGPT_API_MODEL;
         var maxTokens = Integer.parseInt(CHATGPT_API_MAX_TOKENS);
+        var temperature = Float.parseFloat(CHATGPT_API_TEMPERATURE);
 
-        var requestBody = ChatGptRequest.GetChatGptRequestBody(model, userInput, maxTokens);
+        var requestBody = ChatGptRequest.GetChatGptRequestBody(model, userInput, maxTokens, temperature);
 
         try {
-//            var request = HttpRequest.newBuilder()
-//                    .uri(URI.create(url))
-//                    .header("Content-Type", "application/json")
-//                    .header("Authorization", "Bearer " + apiKey)
-//                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
-//                    .build();
-//
-//            var client = HttpClient.newHttpClient();
-//            var response = client.send(request, HttpResponse.BodyHandlers.ofString());
-//
-//            JSONObject resultado = new JSONObject(response.body()).getJSONArray("choices").getJSONObject(0).getJSONObject("message");
-//            var content = new JSONObject(resultado.get("content").toString());
-//
-//            var chatGpt = new ChatGptModel();
-//            chatGpt = gson.fromJson(content.toString(), ChatGptModel.class);
-//            chatGpt.setInputUsuario(userInput);
-//            chatGpt.setResposta(response.body());
-            //return chatGptRepository.save(chatGpt);
+            var request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Bearer " + apiKey)
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    .build();
 
-            return new ChatGptModel();
+            var client = HttpClient.newHttpClient();
+            var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            JSONObject resultado = new JSONObject(response.body()).getJSONArray("choices").getJSONObject(0).getJSONObject("message");
+            var content = new JSONObject(resultado.get("content").toString());
+
+            var chatGpt = new ChatGptModel();
+            chatGpt = om.readValue(content.toString(), ChatGptModel.class);
+            chatGpt.setInputUsuario(userInput);
+            chatGpt.setResposta(response.body());
+            chatGptRepository.save(chatGpt);
+
+            return chatGpt;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
